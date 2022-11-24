49c8f88a2d9e28c05f94e14b26a9ff60e37ba660
Mateusz Wit <maateew@gmail.com>
2022-11-24 16:22:56 +0100

    docs: aar problem

--- android/src/main/java/com/testaars/TestAarsModule.java
+++ android/src/main/java/com/testaars/TestAarsModule.java
@@ -1,9 +1,15 @@
 package com.testaars;
 
+import android.graphics.Bitmap;
+
 import androidx.annotation.NonNull;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.FutureTarget;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.annotations.ReactModule;
+
+import java.util.concurrent.ExecutionException;
 
 @ReactModule(name = TestAarsModule.NAME)
 public class TestAarsModule extends NativeTestAarsSpec {
@@ -24,6 +30,20 @@ public class TestAarsModule extends NativeTestAarsSpec {
   // See https://reactnative.dev/docs/native-modules-android
   @Override
   public double multiply(double a, double b) {
+    FutureTarget<Bitmap> futureTarget =
+      Glide.with(getReactApplicationContext())
+        .asBitmap()
+        .load("https://placekitten.com/g/200/300")
+        .submit();
+
+    try {
+      Bitmap bitmap = futureTarget.get();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
     return a * b;
   }
 }
